b8a93853ca45e7b5f8a522c5f19544cf4bc0f786
kaustubha-chaturvedi <kaustubhachaturvedi@outlook.com>
2021-09-17 11:22:19 +0530

    tailwind css customized

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  purge: [],
+  purge: {
+    preserveHtmlElements: true,
+    content: [
+      './CHP/templates/*.html',
+      './CHP/templates/**/*.html'
+    ]
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
